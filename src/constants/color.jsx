4560ea223a59cef82b915a83fc3482798584e584
greenT-Hee <heehee970322@gmail.com>
2022-12-27 14:18:57 +0900

    Design:네비게이션 제작

--- src/constants/color.jsx
+++ src/constants/color.jsx
@@ -6,7 +6,8 @@ export const MAIN_BACKGROUND_COLOR = 'rgba(226, 192, 250, 0.822)'
 // 네브바
 export const NAV_MAIN_COLOR = 'rgba(226, 192, 250, 0.822)'
 export const NAV_FONT_COLOR = "#fff"
-export const NAV_HOVER_COLOR = '#d293ffd5'
+export const NAV_FONT_HOVER_COLOR = "#b854ff"
+export const NAV_BUTTON_HOVER_COLOR = '#d293ffd5'
 
 
 
