0c2e51a7e80d7c829f05a02813c7392cff90df0b
greenT-Hee <heehee970322@gmail.com>
2022-12-27 17:02:54 +0900

    Create:로그인 화면 제작

--- src/component/buttons/NormalButton.jsx
+++ src/component/buttons/NormalButton.jsx
@@ -1,5 +1,6 @@
 import styled from "styled-components"
 import {NORMAL_BUTTON_COLOR, NORMAL_BUTTON_FONT_COLOR,NORMAL_BUTTON_BORDER_COLOR} from '../../constants/color'
+import {BUTTON_SIZE} from '../../constants/fontSize'
 
 const NormalBtn = styled.button`
   display: block;
@@ -9,6 +10,7 @@ const NormalBtn = styled.button`
   border: 1px solid ${NORMAL_BUTTON_BORDER_COLOR};
   background-color: ${NORMAL_BUTTON_COLOR};
   color:${NORMAL_BUTTON_FONT_COLOR};
+  font-size: ${BUTTON_SIZE};
 `
 
 export default function NormalButton({children}) {
