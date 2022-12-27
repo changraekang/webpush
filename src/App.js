f0da8cd715e97f699381198891771e5076cea4de
changraekang <storkcr@gmail.com>
2022-12-27 10:38:21 +0900

    web push init

--- src/App.js
+++ src/App.js
@@ -1,24 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
-
+import "./App.css";
+import {
+  BrowserRouter,
+  Route,
+  Link,
+  Switch,
+  Router,
+  Routes,
+} from "react-router-dom";
+import Test from "./pages/Test";
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <BrowserRouter>
+        <Routes>
+          {/* 에러페이지 */}
+          <Route path="/test" element={<Test />} />
+        </Routes>
+      </BrowserRouter>
+    </>
   );
 }
 
