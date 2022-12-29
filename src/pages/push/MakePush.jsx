645fdd15a554a00093301e2ae17774e3cd8ac552
changraekang <storkcr@gmail.com>
2022-12-29 10:52:09 +0900

    :poop: Poop

--- src/pages/push/MakePush.jsx
+++ src/pages/push/MakePush.jsx
@@ -1,37 +1,10 @@
-<<<<<<< HEAD
 import React from "react";
-import MakePushBox from "../../component/containers/push/MakePushBox";
 import Layout from "../../templates/Layout";
-=======
-import React from 'react'
-import MakePushBox from '../../components/containers/push/MakePushBox'
-import Layout from '../../templates/Layout'
->>>>>>> 86247ee92baf11894af748cc2b51079ec4407b96
 
 export default function MakePush() {
   return (
     <Layout>
-      <MakePushBox>
-        <p>push</p>
-      </MakePushBox>
-      <MakePushBox>
-        <p>push</p>
-      </MakePushBox>
-      <MakePushBox>
-        <p>push</p>
-        <p>push</p>
-        <p>push</p>
-        <p>push</p>
-        <p>push</p>
-        <p>push</p>
-        <p>push</p>
-        <p>push</p>
-        <p>push</p>
-      </MakePushBox>
-      <MakePushBox>
-        <p>push</p>
-        <p>push</p>
-      </MakePushBox>
+      <p>sda</p>
     </Layout>
   );
 }
