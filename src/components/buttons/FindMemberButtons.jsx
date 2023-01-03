98d3a6aad2007545a2ddc0f45587c09472b1f5bb
changraekang <storkcr@gmail.com>
2023-01-03 17:32:46 +0900

    :sparkles: Create: Email Validation in FindPassword.jsx

--- src/components/buttons/FindMemberButtons.jsx
+++ src/components/buttons/FindMemberButtons.jsx
@@ -1,6 +1,14 @@
-import styled from "styled-components"
-import {ACTIVE_BUTTON_COLOR, ACTIVE_BUTTON_FONT_COLOR,INACTIVE_BUTTON_COLOR, INACTIVE_BUTTON_FONT_COLOR,NORMAL_BUTTON_COLOR, NORMAL_BUTTON_FONT_COLOR,NORMAL_BUTTON_BORDER_COLOR} from '../../constants/color'
-import {BUTTON_SIZE} from '../../constants/fontSize'
+import styled from "styled-components";
+import {
+  ACTIVE_BUTTON_COLOR,
+  ACTIVE_BUTTON_FONT_COLOR,
+  INACTIVE_BUTTON_COLOR,
+  INACTIVE_BUTTON_FONT_COLOR,
+  NORMAL_BUTTON_COLOR,
+  NORMAL_BUTTON_FONT_COLOR,
+  NORMAL_BUTTON_BORDER_COLOR,
+} from "../../constants/color";
+import { BUTTON_SIZE } from "../../constants/fontSize";
 
 const Button = styled.button`
   display: block;
@@ -10,44 +18,46 @@ const Button = styled.button`
   cursor: pointer;
   border-radius: 8px;
   margin-top: 40px;
-  background: ${(props)=>(props.normal ? `${NORMAL_BUTTON_COLOR}` : null)};
+  background: ${(props) => (props.normal ? `${NORMAL_BUTTON_COLOR}` : null)};
   background: ${(props) => (props.active ? `${ACTIVE_BUTTON_COLOR}` : null)};
-  background: ${(props) => (props.inactive ? `${INACTIVE_BUTTON_COLOR}` : null)};
-  color: ${(props)=> (props.normal ? `${NORMAL_BUTTON_FONT_COLOR}` : null)};
+  background: ${(props) =>
+    props.inactive ? `${INACTIVE_BUTTON_COLOR}` : null};
+  color: ${(props) => (props.normal ? `${NORMAL_BUTTON_FONT_COLOR}` : null)};
   color: ${(props) => (props.active ? `${ACTIVE_BUTTON_FONT_COLOR}` : null)};
-  color: ${(props) => (props.inactive ? `${INACTIVE_BUTTON_FONT_COLOR}` : null)};
-  border: 1px solid ${(props)=> (props.normal ? `${NORMAL_BUTTON_BORDER_COLOR}` : "none")};
+  color: ${(props) =>
+    props.inactive ? `${INACTIVE_BUTTON_FONT_COLOR}` : null};
+  border: 1px solid
+    ${(props) => (props.normal ? `${NORMAL_BUTTON_BORDER_COLOR}` : "none")};
 
   &:hover {
-
   }
-`
+`;
 
 // 비밀번호 찾기
-function ActiveFindPasswordButton({children}) {
+function ActiveFindPasswordButton({ children, requestFind }) {
   return (
-    <Button active>{children}</Button>
-  )
+    <Button active onClick={requestFind}>
+      {children}
+    </Button>
+  );
 }
 
-function InactiveFindPasswordButton({children}) {
-  return (
-    <Button inactive>{children}</Button>
-  )
+function InactiveFindPasswordButton({ children }) {
+  return <Button inactive>{children}</Button>;
 }
 
 // 이메일 찾기
-function ActiveFindEmailButton({children}) {
-  return (
-    <Button active>{children}</Button>
-  )
+function ActiveFindEmailButton({ children }) {
+  return <Button active>{children}</Button>;
 }
 
-function InactiveFindEmailButton({children}) {
-  return (
-    <Button inactive>{children}</Button>
-  )
+function InactiveFindEmailButton({ children }) {
+  return <Button inactive>{children}</Button>;
 }
 
-
-export {ActiveFindPasswordButton, InactiveFindPasswordButton, ActiveFindEmailButton, InactiveFindEmailButton}
+export {
+  ActiveFindPasswordButton,
+  InactiveFindPasswordButton,
+  ActiveFindEmailButton,
+  InactiveFindEmailButton,
+};
