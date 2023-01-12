71d6a023b2952907b814d570b0fcd041dff3b83f
greenT-Hee <heehee970322@gmail.com>
2023-01-12 17:35:52 +0900

    :hammer:Add:새 비밀번호 설정 유효성 검사 기능 추가 & recoil 추가

--- src/pages/DashBoard.jsx
+++ src/pages/DashBoard.jsx
@@ -4,7 +4,7 @@ import Layout from "../templates/Layout";
 import styled from "styled-components";
 const PageWrapper = styled.div`
   width: 100%;
-  padding: 200px;
+  padding: 20px;
   display: flex;
   flex-direction: row;
   align-items: center;
